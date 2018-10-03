4e488857b3ad39375ccd7c3e8f690baf1d9792b7
emiliano <emiliano@mildewyPrawn.fciencias.unam.mx>
2018-10-02 23:29:39 -0500

    readmeP03

---
+++ Practicas/Practica3/auto/README.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook
+ "README"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "authblk"
+    "inputenc"
+    "listings")
+   (LaTeX-add-bibitems
+    "lamport94"))
+ :latex)
+
